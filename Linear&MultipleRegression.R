3d2e3c90db16d8b0cbc24312a260b6700a638437
Abhi141188 <abhi141188@gmail.com>
2019-11-30 11:56:24 +0530

    modified on 30 Nov, 2019

--- Linear&MultipleRegression.R
+++ Linear&MultipleRegression.R
@@ -3,13 +3,13 @@ df <- read.csv("F:/R Tool Directory/Choudhary R/stud_reg.csv", header = T)
 View(df)
 df
 
-#Single Linear Regression
+#Single Linear Regression----
 result<-lm(APPLICANTS~PLACE_RATE, data=df)
 summary(result)
 data_fitted<-data.frame(df,fitted.values=fitted(result), residual=resid(result))
 data_fitted
 
-#Multiple Regression
+#Multiple Regression----
 result<-lm(APPLICANTS~PLACE_RATE+NO_GRAD_STUD, data=df)
 summary(result)
 data_fitted<-data.frame(df,fitted.values=fitted(result), residual=resid(result))
@@ -27,7 +27,10 @@ result<-lm(age~hieght)
 summary(result)
 
 #Eq: Age= -100.84 + 1.55*Hieght
+
+#Accuracy:
 #Since R2 is close to 1, hence model is highly significant.
+
 data_fitted<-data.frame(df , fitted.value=fitted(result),residual=resid(result))
 data_fitted
 library("ggplot2")
@@ -41,11 +44,12 @@ plot(mpg~hp, data=mtcars)
 plot(mpg~wt, data=mtcars)
 result<-lm(mpg~hp+wt, data=mtcars)
 summary(result)
-#Accuracy
-#Value of Adjusted R2 = 0.75, means that "75% of the variance in the measure of mpg can be predicted by hp and wt."
+
+#Value of Adjusted R2 = 0.82, 
+#means that "82% of the variance in the measure of mpg can be predicted by hp and wt."
 
 #Checking Multicollinearity:
-result<-lm(mpg~hp+wt+disp+cyl+qsec+gear, data=mtcars)
+result<-lm(mpg~hp+wt+disp+cyl+gear, data=mtcars)
 summary(result)
 
 #install.packages("usdm")
@@ -53,6 +57,51 @@ library(usdm)
 vif(mtcars)
 vif(mtcars[,c(-3)])
 vif(mtcars[,c(-3,-2)])
-result<-lm(mpg~hp+wt+qsec+gear, data=mtcars)
+result<-lm(mpg~hp+wt, data=mtcars)
 summary(result)
+
+#----Example-3
+#Create Training and Test data ----
+trainingRowIndex <- sample(1:nrow(mtcars), 0.8*nrow(mtcars))  # row indices for training data
+trainingData <- mtcars[trainingRowIndex, ]  # model training data
+testData  <- mtcars[-trainingRowIndex, ]   # test data
+
+#Bi-variate Analysis:----
+#Checking relationships between different variables
+pairs(mtcars)
+
+#Correlation
+cr = cor(mtcars)
+cr
+library(corrplot)
+corrplot(cr,type="lower",method="circle")
+corrplot(cr,type="lower",method="number")
+
+#Build the model on training data----
+lmMod <- lm(mpg ~ cyl+ #disp
+              #+hp
+              +wt, data=trainingData)  # build the model
+# Review diagnostic measures
+summary (lmMod)  # model summary
+
+#Accuracy:
+#Since R2 is close to 1, i.e., 0.84, hence model is significant.
+
+#Prediction----
+#Predicitng values for test dataset
+testData$mpgPred <- predict(lmMod, testData)
+View(testData)
+#Accuracy:----
+#Determining Prediction accuracy on test dataset using MAPE
+
+#MAPE(MeanAbsolutePercentageError): 
+#Lower its value better is the accuracy of the model.
+#MAPE Calculation:
+mape <- mean(abs((testData$mpgPred - testData$mpg))/testData$mpg)
+mape
+
+# Mape using mape function
+#install.packages("Metrics")
+library(Metrics)
+mape(testData$mpgPred,testData$mpg)
 
