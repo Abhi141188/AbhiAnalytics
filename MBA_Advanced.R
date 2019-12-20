8848b284b567849519235117e96ccf5fcbe041c7
Abhi141188 <abhi141188@gmail.com>
2019-12-20 23:14:02 +0530

    Dec, 2019

--- MBA_Advanced.R
+++ MBA_Advanced.R
@@ -36,65 +36,74 @@ itemFrequency(dt2, type = "relative")
 itemFrequencyPlot(dt2,topN = 5)
 
 # aggregated data
-rules = apriori(dt2, parameter=list(support=0.05, confidence=0.8))
-rules = apriori(dt2, parameter=list(support=0.005, confidence=0.8, minlen = 3))
-rules = apriori(dt2, parameter=list(support=0.005, confidence=0.8, maxlen = 4))
+rules1 = apriori(dt2, parameter=list(support=0.05, confidence=0.8))
+summary(rules1)
+inspect(rules1[1:5])
+
+rules2 = apriori(dt2, parameter=list(support=0.005, confidence=0.8, minlen = 3))
+summary(rules2)
+inspect(rules2[1:5])
+
+rules3 = apriori(dt2, parameter=list(support=0.005, confidence=0.8, maxlen = 4))
+summary(rules3)
+inspect(rules3[1:5])
 
 #Convert rules into data frame
-rules3 = as(rules, "data.frame")
-View(rules3)
-write(rules, "F:/rWork/rProjects/AbhiAnalytics/rules2.csv", sep=",")
+rules4 = as(rules3, "data.frame")
+View(rules4)
+write(rules4, "F:/rWork/rProjects/AbhiAnalytics/rules2.csv", sep=",")
 
 # Show only particular product rules ( let say Product H)
-inspect( subset( rules, subset = rhs %pin% "Product H" ))
+inspect( subset( rules3, subset = rhs %pin% "Product H" ))
+inspect( subset( rules3, subset = lhs %pin% "Product H" ))
 
 # Show the top 10 rules
 options(digits=2)
 #Option(digit=2): this cmd controls the number of significant digits to print when 
 #printing numeric values. 
-inspect(rules[1:10])
+inspect(rules3[1:10])
 
 # Get Summary Information
-summary(rules)
+summary(rules3)
 
 # Sort by Lift
-rules<-sort(rules, by="lift", decreasing=TRUE)
+rules3<-sort(rules3, by="lift", decreasing=TRUE)
 
 # Remove Unnecessary Rules
 #identify duplicate rules and remove them
-rules
-redundant_rules <- is.redundant(rules)
+rules3
+redundant_rules <- is.redundant(rules3)
 redundant_rules
 summary(redundant_rules)
 #Since we got TRUE for 674 rules,Hence, there are 674 rules which are redundant.
 #We will have to remove these duplicate rules.
-rules<-rules[!redundant_rules]
-rules
+rules3<-rules3[!redundant_rules]
+rules3
 # We have removed the duplicate rules from above cmds.
 
-inspect(rules[1:5])
+inspect(rules3[1:5])
 
 #Visualization of Rules
 #install.packages("arulesViz")
 library("arulesViz")
-plot(rules,method = "graph")
-plot(rules[1:50],method = "graph")
+plot(rules3,method = "graph")
+plot(rules3[1:50],method = "graph")
 
 #Interactive plot
-plot(rules,method = "graph", interactive = T)
-plot(rules[1:25],method = "graph", interactive = T)
+plot(rules3,method = "graph", interactive = T)
+plot(rules3[1:25],method = "graph", interactive = T)
 
 #What are customers likely to buy before they purchase "Product A"
-rules<-apriori(data=dt, parameter=list(supp=0.001,conf = 0.8), 
+rules_A<-apriori(data=dt, parameter=list(supp=0.001,conf = 0.8), 
                appearance = list(default="lhs",rhs="Product A"),
                control = list(verbose=F))
-rules<-sort(rules, decreasing=TRUE,by="confidence")
-inspect(rules[1:5])
+rules_A<-sort(rules_A, decreasing=TRUE,by="confidence")
+inspect(rules_A)
 
 #What are customers likely to buy if they purchased "Product A"l
-rules<-apriori(data=dt, parameter=list(supp=0.001,conf = 0.8), 
+rules_PA<-apriori(data=dt, parameter=list(supp=0.001,conf = 0.8), 
                appearance = list(default="rhs",lhs="Product A"),
                control = list(verbose=F))
-rules<-sort(rules, decreasing=TRUE,by="confidence")
-inspect(rules)
+rules_PA<-sort(rules_PA, decreasing=TRUE,by="confidence")
+inspect(rules_PA)
 
